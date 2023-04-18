a3d1b4daa9bda9a2d411650ca2f6973ce692d700
Raphael Kirschner <raphael.kirschner@gmail.com>
2023-04-18 15:42:02 -0300

    first commit

---
+++ src/main/java/br/com/decolar/reembolso/ReembolsoApplication.java
@@ -0,0 +1,13 @@
+package br.com.decolar.reembolso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReembolsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReembolsoApplication.class, args);
+	}
+
+}
